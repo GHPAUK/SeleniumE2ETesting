2d385c4d1b53f0652194bf1bc6d1ee7db16eaba4
GH <georgeheath.98840@gmail.com>
2022-06-07 14:48:45 +0100

    GH: Pushing a working(flaky) end to end test for the clothes site

--- src/test/java/seleniumTesting/clothesSite/ClothesSiteTest.java
+++ src/test/java/seleniumTesting/clothesSite/ClothesSiteTest.java
@@ -31,7 +31,7 @@ public class ClothesSiteTest {
   }
 
   @Test
-  void navigateToCasualDressesAndAddOneToBasket() throws InterruptedException {
+  void navigateToCasualDressesAndAddOneToBasketCreateAnAccount() throws InterruptedException {
 
     driver.navigate().to( "http://automationpractice.com/index.php" );
     Thread.sleep( 10000 );
@@ -68,11 +68,10 @@ public class ClothesSiteTest {
     Thread.sleep( 5000 );
     driver.findElement( By.cssSelector( ".standard-checkout" ) ).click();
     Thread.sleep( 10000 );
-    driver.findElement( By.id( "email_create" ) ).sendKeys( "testingTesting123@test.com" );
+    driver.findElement( By.id( "email_create" ) ).sendKeys( "testingTesting12345@test.com" );
     Thread.sleep( 1000 );
-    //    driver.findElement( new By.ById("email") ).sendKeys( "testing@test.com" );
     driver.findElement( By.id( "SubmitCreate" ) ).click();
-    Thread.sleep( 5000 );
+    Thread.sleep( 10000 );
     driver.findElement( By.id( "id_gender1" ) ).click();
     Thread.sleep( 500 );
     driver.findElement( By.id( "customer_firstname" ) ).sendKeys( "John" );
@@ -103,14 +102,10 @@ public class ClothesSiteTest {
     driver.findElement( By.id( "city" ) ).sendKeys( "Mantester" );
     Thread.sleep( 500 );
     driver.findElement( By.id( "id_state" ) ).click();
-    Thread.sleep( 500 );
-    driver.findElement( By.cssSelector( "#id_state > option:nth-child(52)" ) ).click();
-    Thread.sleep( 500 );
+    Thread.sleep( 600 );
+    driver.findElement( By.cssSelector( "#id_state > option:nth-child(3)" ) ).click();
+    Thread.sleep( 600 );
     driver.findElement( By.id( "postcode" ) ).sendKeys( "00000" );
-    Thread.sleep( 500 );
-    driver.findElement( By.id( "id_country" ) ).click();
-    Thread.sleep( 500 );
-    driver.findElement( By.cssSelector( "#id_country > option:nth-child(2)" ) ).click();
     Thread.sleep( 500 );
     driver.findElement( By.id( "phone_mobile" ) ).sendKeys( "07654245721" );
     Thread.sleep( 500 );
@@ -126,6 +121,66 @@ public class ClothesSiteTest {
     WebElement bank = hookPayment.findElement( By.className( "bankwire" ) );
     Thread.sleep( 1000 );
     bank.click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.cssSelector( "button.button-medium" ) ).click();
+  }
+
+  @Test
+  void navigateToCasualDressesAndAddOneToBasketSignIn() throws InterruptedException {
+
+    driver.navigate().to( "http://automationpractice.com/index.php" );
+    Thread.sleep( 10000 );
+    WebElement topBlockMenu = driver.findElement( By.id( "block_top_menu" ) );
+    WebElement menu = topBlockMenu.findElement( By.className( "menu-content" ) );
+    WebElement dresses = menu.findElement( By.cssSelector( ".sf-menu > li:nth-child(2) > a:nth-child(1)" ) );
+    dresses.click();
+    Thread.sleep( 7000 );
+    driver.findElement( By.cssSelector( "#subcategories > ul:nth-child(2) > li:nth-child(3) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)" ) ).click();
+    Thread.sleep( 7000 );
+    driver.findElement( By.cssSelector( "li.ajax_block_product:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)" ) ).click();
+    Thread.sleep( 7000 );
+    driver.findElement( By.cssSelector( "#thumbnail_16 > a:nth-child(1)" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( ".fancybox-item" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( "#thumbnail_17 > a:nth-child(1)" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( ".fancybox-item" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( "#thumbnail_18 > a:nth-child(1)" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( ".fancybox-item" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.id( "group_1" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.cssSelector( "#group_1 > option:nth-child(2)" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.id( "color_8" ) ).click();
+    Thread.sleep( 2000 );
+    driver.findElement( By.id( "add_to_cart" ) ).click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.cssSelector( "a.btn:nth-child(2)" ) ).click();
+    Thread.sleep( 5000 );
+    driver.findElement( By.cssSelector( ".standard-checkout" ) ).click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.id( "email" ) ).sendKeys( "testingTesting123@test.com" );
+    Thread.sleep( 1000 );
+    driver.findElement( By.id( "passwd" ) ).sendKeys( "SuperSafe1" );
+    Thread.sleep( 1000 );
+    driver.findElement( By.id( "SubmitLogin" ) ).click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.cssSelector( "button.button:nth-child(4)" ) ).click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.id( "cgv" ) ).click();
+    Thread.sleep( 1000 );
+    driver.findElement( By.cssSelector( "button.button:nth-child(4)" ) ).click();
+    Thread.sleep( 10000 );
+    WebElement hookPayment = driver.findElement( By.id( "HOOK_PAYMENT" ) );
+    WebElement bank = hookPayment.findElement( By.className( "bankwire" ) );
+    Thread.sleep( 1000 );
+    bank.click();
+    Thread.sleep( 10000 );
+    driver.findElement( By.cssSelector( "button.button-medium" ) ).click();
   }
 
   //  List<WebElement> dresses = driver.findElements( By.className( "product-container" ) );
